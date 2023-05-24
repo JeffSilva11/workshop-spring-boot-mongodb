607e048137f037268ac3566f4e8668e1eecef1f3
Jefferson Silva <fj_adm@hotmail.com>
2023-05-24 20:57:21 -0300

    Obtendo um usuário por id

--- src/main/java/curso/java/curso_336_projeto_mogodb/services/UserService.java
+++ src/main/java/curso/java/curso_336_projeto_mogodb/services/UserService.java
@@ -1,12 +1,14 @@
 package curso.java.curso_336_projeto_mogodb.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import curso.java.curso_336_projeto_mogodb.domain.User;
 import curso.java.curso_336_projeto_mogodb.repository.UserRepository;
+import curso.java.curso_336_projeto_mogodb.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -16,5 +18,10 @@ public class UserService {
 
 	public List<User> findAll() {
 		return repo.findAll();
+	}
+	
+	public User findById(String id) {
+		Optional<User> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
 	}
 }
---
+++ src/main/java/curso/java/curso_336_projeto_mogodb/services/exception/ObjectNotFoundException.java
@@ -0,0 +1,9 @@
+package curso.java.curso_336_projeto_mogodb.services.exception;
+
+public class ObjectNotFoundException extends RuntimeException {
+	private static final long serialVersionUID = 1L;
+
+	public ObjectNotFoundException(String msg) {
+		super(msg);
+	}
+}
