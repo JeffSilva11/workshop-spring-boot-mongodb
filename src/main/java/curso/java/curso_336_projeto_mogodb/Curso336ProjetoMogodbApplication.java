22faeab45343cfe006bd742ae87035a19d4f6032
Jefferson Silva <fj_adm@hotmail.com>
2023-05-23 21:18:32 -0300

    Primeiro commit - projeto criado

---
+++ src/main/java/curso/java/curso_336_projeto_mogodb/Curso336ProjetoMogodbApplication.java
@@ -0,0 +1,13 @@
+package curso.java.curso_336_projeto_mogodb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Curso336ProjetoMogodbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Curso336ProjetoMogodbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/curso/java/curso_336_projeto_mogodb/Curso336ProjetoMogodbApplicationTests.java
@@ -0,0 +1,13 @@
+package curso.java.curso_336_projeto_mogodb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Curso336ProjetoMogodbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
