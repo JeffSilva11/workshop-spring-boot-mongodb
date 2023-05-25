4b40c3e666689b14cb3d026295477b14783d4ee5
Jefferson Silva <fj_adm@hotmail.com>
2023-05-25 20:38:32 -0300

    Obtendo um post por id

---
+++ src/main/java/curso/java/curso_336_projeto_mogodb/services/PostService.java
@@ -0,0 +1,22 @@
+package curso.java.curso_336_projeto_mogodb.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import curso.java.curso_336_projeto_mogodb.domain.Post;
+import curso.java.curso_336_projeto_mogodb.repository.PostRepository;
+import curso.java.curso_336_projeto_mogodb.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+
+	@Autowired
+	private PostRepository repo;
+
+	public Post findById(String id) {
+		Optional<Post> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+}
