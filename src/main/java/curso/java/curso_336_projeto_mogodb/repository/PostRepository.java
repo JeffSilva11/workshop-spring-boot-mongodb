9c871913b1202835003ca9aec0dc5192007f0cb2
Jefferson Silva <fj_adm@hotmail.com>
2023-05-25 21:05:25 -0300

    Consulta simples com query methods

--- src/main/java/curso/java/curso_336_projeto_mogodb/repository/PostRepository.java
+++ src/main/java/curso/java/curso_336_projeto_mogodb/repository/PostRepository.java
@@ -1,5 +1,7 @@
 package curso.java.curso_336_projeto_mogodb.repository;
 
+import java.util.List;
+
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,5 +9,7 @@ import curso.java.curso_336_projeto_mogodb.domain.Post;
 
 @Repository
 public interface PostRepository extends MongoRepository<Post, String> {
+	
+	List<Post> findByTitleContainingIgnoreCase(String text);
 
 }
